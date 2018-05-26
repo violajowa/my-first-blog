b628e7131933be310902dd5c745a9cab7411db22
Viola_Jowa <viola.jowa@gmail.com>
2018-05-26 12:23:18 +0200

    My Django Girls app, first commit

---
+++ hey.py
@@ -0,0 +1,7 @@
+def hi(name):
+	if name == 'Viola'
+		print('Hi Vee')
+	elif name == 'Jubilent'
+		print('Hi Jubi')
+	else:
+		print('Hi anonymous')
---
+++ hi.py
@@ -0,0 +1,9 @@
+def hi(name):
+	if name == 'Viola':
+		print('Hi Vee')
+	elif name == 'Jubilent':
+		print('Hi Jubi')
+	else:
+		print('Hi anonymous')
+
+hi("Viola")
